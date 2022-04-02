e4269d172719e92f2bd7806757112f44a0ac4178
chanhl22 <chanhlee@student.42seoul.kr>
2022-04-02 17:38:24 +0900

    람다 표현식

---
+++ src/lambda/Foo.java
@@ -0,0 +1,62 @@
+package lambda;
+
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        foo.run();
+    }
+
+    private void run() {
+        int baseNumber = 10;
+        //baseNumber++; //불가능
+
+        // 로컬 클래스
+        class LocalClass {
+            void printBaseNumber() {
+                System.out.println(baseNumber);
+            }
+        }
+
+        // 로컬 클래스
+        class LocalClassAnother {
+            void printBaseNumber() {
+                int baseNumber = 11;
+                System.out.println(baseNumber); //11
+            }
+        }
+
+        // 익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(baseNumber);
+            }
+        };
+
+        // 익명 클래스
+        Consumer<Integer> integerConsumerAnother = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer baseNumber) {
+                System.out.println(baseNumber); //11
+            }
+        };
+
+
+        // 람다
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNumber);
+        };
+
+        //불가능
+        //IntConsumer printIntAnother = (baseNumber) -> {
+        //    System.out.println(baseNumber);
+        //};
+
+        printInt.accept(10);
+    }
+
+}
