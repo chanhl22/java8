c900ad0ad0480ec65c97508c862ca2a1b1518ae5
chanhl22 <lch9502@naver.com>
2022-08-19 10:40:10 +0900

    ㅇDate와  ìTime 소개

---
+++ src/date_time/UsedBeforeDate.java
@@ -0,0 +1,25 @@
+package date_time;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class UsedBeforeDate {
+    public static void main(String[] args) throws InterruptedException {
+        Date date = new Date();
+        long time = date.getTime();
+        System.out.println(date);
+        System.out.println(time);
+        Calendar calendar = new GregorianCalendar();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+
+        Thread.sleep(1000 * 3);
+        Date after3Seconds = new Date();
+        System.out.println(after3Seconds);
+        after3Seconds.setTime(time);
+        System.out.println(after3Seconds);
+
+        Calendar birthday = new GregorianCalendar(1995, Calendar.FEBRUARY, 8);
+    }
+}
