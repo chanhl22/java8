1f2a009680b30af6d1f1e84a5eaab80119b8c8c6
chanhl22 <lch9502@naver.com>
2024-06-24 23:34:02 +0900

    feat: completedFuture allOf

---
+++ src/chapter6/section4/CompletableFutureAllOf.java
@@ -0,0 +1,22 @@
+package chapter6.section4;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureAllOf {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "Hello";
+        });
+
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "World";
+        });
+
+        CompletableFuture<String> future = hello.thenCombine(world, (h, w) -> h + " " + w);
+        System.out.println(future.get());
+    }
+
+}
