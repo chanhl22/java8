55e5c882f8413ec01d06e7fc13605eada2868950
chanhl22 <lch9502@naver.com>
2024-08-13 10:55:21 +0900

    anyOf

--- src/chapter6/section4/CompletableFutureAnyOf.java
+++ src/chapter6/section4/CompletableFutureAnyOf.java
@@ -1,9 +1,7 @@
 package chapter6.section4;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 public class CompletableFutureAnyOf {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -17,17 +15,8 @@ public class CompletableFutureAnyOf {
             return "World";
         });
 
-        List<CompletableFuture<String>> futures = List.of(hello, world);
-        CompletableFuture[] futuresArray = futures.toArray(new CompletableFuture[futures.size()]);
-
-        CompletableFuture<List<String>> results = CompletableFuture.allOf(futuresArray)
-                .thenApply(result ->
-                        futures.stream()
-                                .map(CompletableFuture::join)
-                                .collect(Collectors.toList())
-                );
-
-        results.get().forEach(System.out::println);
+        CompletableFuture<Void> future = CompletableFuture.anyOf(hello, world).thenAccept(System.out::println);
+        future.get();
     }
 
 }
