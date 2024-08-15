3d11b31ab113c2ef30a2ac4a678d1028f94c1ebc
chanhl22 <lch9502@naver.com>
2024-08-15 11:16:41 +0900

    handle

---
+++ src/chapter6/section4/CompletableFutureHandle.java
@@ -0,0 +1,29 @@
+package chapter6.section4;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureHandle {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        boolean throwError = true;
+
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
+            if (throwError) {
+                throw new IllegalArgumentException();
+            }
+
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "Hello";
+        }).handle((result, ex) -> {
+            if (ex != null) {
+                System.out.println(ex);
+                return "Error!";
+            }
+
+            return result;
+        });
+
+        System.out.println(hello.get());
+    }
+
+}
